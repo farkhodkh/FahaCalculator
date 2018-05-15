420cc086130a1674b6f6dd5f152fcc5865c6767e
Farhod.Haknazarov <986532qw@gmail.com>
2018-05-15 11:11:30 +0300

    sixth day

--- app/src/main/java/com/example/user/fahacalculator/calculation/Calculator.java
+++ app/src/main/java/com/example/user/fahacalculator/calculation/Calculator.java
@@ -150,72 +150,73 @@ public class Calculator {
         }
     }
 
+    @Deprecated
     public void inputData(String btnTag) {
-//        if (btnTag == null || btnTag.equals("")) {
-//            return;
-//        }
-//
-//        if (btnTag == CalculatorParameters.DEL_BUTTON) {
-//            if (inputList.size() > 0) {
-//                inputList.remove(inputList.size() - 1);
-//            }
-//            //TODO Удаление элементов из трех массивов синхронизировать!
-//            if ((rpnArrayList.size() == 0 && lastStack == 1) || (stackArray.size() == 0 && lastStack == 2)) {
-//                return;
-//            }
-//
-//            if (lastStack == 1 || stackArray.size() == 0) {
-//                rpnArrayList.remove(rpnArrayList.size() - 1);
-//                lastStack = 2;
-//            } else if (lastStack == 2) {
-//                stackArray.remove(stackArray.size() - 1);
-//                lastStack = 1;
-//            }
-//
-//            return;
-//        }
+        if (btnTag == null || btnTag.equals("")) {
+            return;
+        }
+
+        if (btnTag == CalculatorParameters.DEL_BUTTON) {
+            if (inputList.size() > 0) {
+                inputList.remove(inputList.size() - 1);
+            }
+            //TODO Удаление элементов из трех массивов синхронизировать!
+            if ((rpnArrayList.size() == 0 && lastStack == 1) || (stackArray.size() == 0 && lastStack == 2)) {
+                return;
+            }
+
+            if (lastStack == 1 || stackArray.size() == 0) {
+                rpnArrayList.remove(rpnArrayList.size() - 1);
+                lastStack = 2;
+            } else if (lastStack == 2) {
+                stackArray.remove(stackArray.size() - 1);
+                lastStack = 1;
+            }
+
+            return;
+        }
 
         if (thisIsNumber(btnTag)) {
-//            if (lastStack == 1) {
-//                String uppItem = rpnArrayList.get(rpnArrayList.size() - 1) + btnTag;
-//
-//                rpnArrayList.add(rpnArrayList.size() - 1, uppItem);
-//                rpnArrayList.remove(rpnArrayList.size() - 1);
-//
-//                inputList.add(inputList.size() - 1, uppItem);
-//                inputList.remove(inputList.size() - 1);
-//            } else {
-//                rpnArrayList.add(btnTag);
-//                inputList.add(btnTag);
-//            }
-//            lastStack = 1;
+            if (lastStack == 1) {
+                String uppItem = rpnArrayList.get(rpnArrayList.size() - 1) + btnTag;
+
+                rpnArrayList.add(rpnArrayList.size() - 1, uppItem);
+                rpnArrayList.remove(rpnArrayList.size() - 1);
+
+                inputList.add(inputList.size() - 1, uppItem);
+                inputList.remove(inputList.size() - 1);
+            } else {
+                rpnArrayList.add(btnTag);
+                inputList.add(btnTag);
+            }
+            lastStack = 1;
         } else {
 
-//            if (stackArray.size() == 0 || btnTag.equals("(")) {
-//                stackArray.add(btnTag);
-//                inputList.add(btnTag);
-//                lastStack = 2;
-//                return;
-//            }
+            if (stackArray.size() == 0 || btnTag.equals("(")) {
+                stackArray.add(btnTag);
+                inputList.add(btnTag);
+                lastStack = 2;
+                return;
+            }
 
             int stackSize = stackArray.size() - 1;
             String lastItem = stackArray.get(stackSize);
 
             if (btnTag.equals(")")) {
-//                boolean check = true;
-//                lastItem = stackArray.get(stackSize);
-//                do {
-//                    if (lastItem.equals("(")) {
-//                        stackArray.remove(stackSize);
-//                        check = false;
-//                    }
-//
-//                    rpnArrayList.add(lastItem);
-//                    stackArray.remove(stackSize);
-//                    stackSize--;
-//                    lastStack = 1;
-//                }
-//                while (check);
+                boolean check = true;
+                lastItem = stackArray.get(stackSize);
+                do {
+                    if (lastItem.equals("(")) {
+                        stackArray.remove(stackSize);
+                        check = false;
+                    }
+
+                    rpnArrayList.add(lastItem);
+                    stackArray.remove(stackSize);
+                    stackSize--;
+                    lastStack = 1;
+                }
+                while (check);
             } else if (operatorPriority(lastItem) >= operatorPriority(btnTag)) {
                 rpnArrayList.add(lastItem);
                 stackArray.remove(stackSize);
